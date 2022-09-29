574f351e24aae06474071f4ea3850eb75397c742
mobak88 <mostafa0401@ymail.com>
2022-09-29 23:42:16 +0200

    Checking bug on deplyment

--- src/scoreBoard/ScoreBoard.jsx
+++ src/scoreBoard/ScoreBoard.jsx
@@ -24,6 +24,7 @@ const ScoreBoard = ({ score, computerScore }) => {
     }
     setShowSubmitScore((prevState) => !prevState);
     setSubmittedScore((prevState) => !prevState);
+    console.log(submittedScore);
   };
 
   return (
